b77be00f615c30e6bc776bf942c07642f7dbd89d
leonardo33008 <leonardooortiz@gmail.com>
2020-04-23 12:40:09 -0300

    incluindo tela de selecao de lugar e incluindo imagem da capa do filme

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,11 +3,14 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Sessao {
@@ -65,6 +68,10 @@ public class Sessao {
 
     public Filme getFilme() {
         return this.filme;
+    }
+
+    public Map<String, List<Lugar>> getMapaDeLugares(){ 
+        return sala.getMapaDeLugares();
     }
 
 }
