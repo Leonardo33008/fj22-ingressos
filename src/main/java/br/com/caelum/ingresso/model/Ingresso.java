665ee505886905dc0f2e9c7ee58a11614eb611b8
leonardo33008 <leonardooortiz@gmail.com>
2020-04-23 20:46:30 -0300

    inclui tipo de desconto e adapta os testes para tipo de ingresso

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -3,18 +3,67 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import br.com.caelum.ingresso.model.descontos.Desconto;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+
+
+@Entity
 public class Ingresso {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne
 	private Sessao sessao;
+
 	private BigDecimal preco;
-	
-	public Ingresso (Sessao sessao, Desconto tipoDeDesconto) {
-		this.setSessao(sessao);
-		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+
+	@ManyToOne
+	private Lugar lugar;
+
+	@Enumerated(EnumType.STRING)
+	private TipoDeIngresso tipoDeIngresso;
+
+
+	/**
+ 	* @deprecated hibernate only
+ 	*/
+	public Ingresso(){
+
 	}
-	
+
+
+	public Ingresso(Sessao sessao, TipoDeIngresso tipoDeIngresso, Lugar lugar) {
+		this.sessao = sessao;
+		this.setTipoDeIngresso(tipoDeIngresso);
+		this.preco = tipoDeIngresso.aplicaDesconto(sessao.getPreco());
+
+		this.setLugar(lugar);
+	}
+
+	public TipoDeIngresso getTipoDeIngresso() {
+		return tipoDeIngresso;
+	}
+
+	public void setTipoDeIngresso(TipoDeIngresso tipoDeIngresso) {
+		this.tipoDeIngresso = tipoDeIngresso;
+	}
+
+	public Lugar getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
+	}
+
 	public BigDecimal getPreco() {
 		return preco.setScale(2, RoundingMode.HALF_UP);
 	}
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,13 +3,17 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 
 @Entity
@@ -29,6 +33,10 @@ public class Sessao {
     
     private BigDecimal preco;
 
+    @OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+    private Set<Ingresso> ingressos = new HashSet<>();
+ 
+
     /*
         * Usado apenas pelo Hibernate, na criacao da tabela
     */
@@ -45,6 +53,12 @@ public class Sessao {
         this.preco = sala.getPreco().add(filme.getPreco());
         
     }
+
+    public boolean isDisponivel(Lugar lugarSelecionado) {
+        return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> 
+                                            lugar.equals(lugarSelecionado));
+    }
+
     
     public BigDecimal getPreco() {
     	return preco.setScale(2, RoundingMode.HALF_UP);
@@ -72,6 +86,14 @@ public class Sessao {
 
     public Map<String, List<Lugar>> getMapaDeLugares(){ 
         return sala.getMapaDeLugares();
+    }
+
+    public Set<Ingresso> getIngressos() {
+        return this.ingressos;
+    }
+
+    public void setIngressos(Set<Ingresso> ingressos) {
+        this.ingressos = ingressos;
     }
 
 }
